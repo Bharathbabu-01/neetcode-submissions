054594646e9e0669958fc144f946d2dcc8de1ec3
Bharath Babu Jagarlamudi <140453571+Bharathbabu-01@users.noreply.github.com>
2026-05-22 14:46:14 -0700

    Add: python-elif - submission-3

---
+++ Python For Beginners/python-elif/submission-3.py
@@ -0,0 +1,15 @@
+def check_range(num: int) -> str:
+    if num < 0:
+        return str("negative")
+    elif num == 0:
+        return str("zero")
+    elif (num > 0) & (num < 10):
+        return str("positive single digit")
+    else:
+        return str("positive multi digit")
+  
+# don't modify code below this line
+print(check_range(-10))
+print(check_range(0))
+print(check_range(9))
+print(check_range(1000))
